1d4e7f2678efe1eb8abe314ff397a5695b15e947
Felipe Antunes <121889187+Felipe-Antunes-IFMG@users.noreply.github.com>
2023-01-05 12:26:12 -0300

    v3

--- pg1/script_pg1.js
+++
@@ -1,4 +0,0 @@
-var botao = document.querySelector('button');
-botao.addEventListener("click",function(){
-    window.open("file:///C:/Users/antun/Documents/Estudos/Feira_de_Ciencias/index.html#",'_self')
-});
--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var res1 = document.querySelectorAll('div>p>a');
 for(var i = 0;i<res1.length;i++){
     res1[i].onclick = function(){
         var num = Math.floor(Math.random()*3 + 1);
-        window.open(`file:///C:/Users/antun/Documents/Estudos/Feira_de_Ciencias/pg${num}/pg${num}.html`,'_self')
+        window.open(`pg${num}/pg${num}.html`,'_self')
         
     }
 }
