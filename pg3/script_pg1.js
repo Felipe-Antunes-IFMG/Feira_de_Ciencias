a760b2b57deba85154bdc810f6f589515e1725e8
Felipe Antunes <121889187+Felipe-Antunes-IFMG@users.noreply.github.com>
2023-01-05 11:54:17 -0300

    v2

---
+++ pg3/script_pg1.js
@@ -0,0 +1,4 @@
+var botao = document.querySelector('button');
+botao.addEventListener("click",function(){
+    window.open("file:///C:/Users/antun/Documents/Estudos/Feira_de_Ciencias/index.html#",'_self')
+});
--- script.js
+++ script.js
@@ -1,7 +1,9 @@
-let res = document.getElementById('text1');
-function texto1(){
-    res.style.backgroundColor = 'red';
-}
-function texto2(){
-    res.style.backgroundColor = 'blue';
+var res1 = document.querySelectorAll('div>p>a');
+
+for(var i = 0;i<res1.length;i++){
+    res1[i].onclick = function(){
+        var num = Math.floor(Math.random()*3 + 1);
+        window.open(`file:///C:/Users/antun/Documents/Estudos/Feira_de_Ciencias/pg${num}/pg${num}.html`,'_self')
+        
+    }
 }
